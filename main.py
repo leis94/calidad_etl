3d086be6d2d7a560decba5ba2809803bed3f63d9
Daniel Silva <bukaro.dany@gmail.com>
2021-06-03 14:03:37 -0500

    Updating main.py

--- main.py
+++ main.py
@@ -6,20 +6,24 @@ from avaya.llamadas import av_llamadas
 from avaya.abandonos import av_abandonos
 from avaya.poblar_dims_avaya import dims_avaya_llamadas, dims_avaya_abandonos
 from avaya.fact_avaya import fact_av_llamadas, fact_av_abandonos
+import time
 
 
 if __name__ == '__main__':
+    start = time.time()
     print("Inicio")
-    # clic_abiertos()
-    # clic_resueltos()
-    # dimensiones_clic_abiertos()
-    # dimensiones_clic_resueltos()
-    # fact_clic_abiertos()
-    # fact_clic_resueltos()
+    clic_abiertos()
+    clic_resueltos()
+    dimensiones_clic_abiertos()
+    dimensiones_clic_resueltos()
+    fact_clic_abiertos()
+    fact_clic_resueltos()
     av_llamadas()
     av_abandonos()
     dims_avaya_llamadas()
     dims_avaya_abandonos()
     fact_av_llamadas()
     fact_av_abandonos()
+    end = time.time()
+    print(end - start)
     print("Finalizó")
