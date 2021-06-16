08736b294e020e2fce7f88010b6283c3a934f456
Daniel Silva <bukaro.dany@gmail.com>
2021-06-16 09:02:12 -0500

    Actualizando nombre bd en sm

--- service_manager/sm_cerrado.py
+++ service_manager/sm_cerrado.py
@@ -16,7 +16,7 @@ def sm_cerrado():
     file_name = path_leaf(path)
     mover_archivo(file_name)
 
-    columns_names_sql = """SHOW columns FROM calidad_process.sm_cerrado;"""
+    columns_names_sql = """SHOW columns FROM calidad_etl.sm_cerrado;"""
     df_sql = pd.read_sql(columns_names_sql, conn.conecction_db())
     columns_bd = df_sql.iloc[0:, [0]]
     columns_bd = columns_bd['Field'].tolist()
@@ -24,7 +24,7 @@ def sm_cerrado():
     # Cambio las columnas del dataframe que están con espacios por _ las cuales son las de la BD.
     df_exel_backlog_cerrado.columns = df_exel_backlog_cerrado.columns[:0].tolist(
     ) + columns_bd
-    sm_backlog_sql = """SELECT * FROM calidad_process.sm_cerrado LIMIT 1;"""
+    sm_backlog_sql = """SELECT * FROM calidad_etl.sm_cerrado LIMIT 1;"""
     df_sql = pd.read_sql(sm_backlog_sql, conn.conecction_db())
 
     if not df_sql.empty:
