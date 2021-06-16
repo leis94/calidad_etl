9c4926e84bcdf16ad592a683ec793cc205cdc466
Daniel Silva <bukaro.dany@gmail.com>
2021-06-16 08:43:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from clic.clic_abiertos import clic_abiertos
 from clic.clic_resueltos import clic_resueltos
 from clic.poblar_dims_clic import dimensiones_clic_abiertos, dimensiones_clic_resueltos
-# from clic.fact_clic import fact_clic_abiertos, fact_clic_resueltos
-# from avaya.llamadas import av_llamadas
-# from avaya.abandonos import av_abandonos
-# from avaya.poblar_dims_avaya import dims_avaya_llamadas, dims_avaya_abandonos
-# from avaya.fact_avaya import fact_av_llamadas, fact_av_abandonos
+from clic.fact_clic import fact_clic_abiertos, fact_clic_resueltos
+from avaya.llamadas import av_llamadas
+from avaya.abandonos import av_abandonos
+from avaya.poblar_dims_avaya import dims_avaya_llamadas, dims_avaya_abandonos
+from avaya.fact_avaya import fact_av_llamadas, fact_av_abandonos
 from service_manager.sm_backlog import sm_backlog
 from service_manager.sm_cerrado import sm_cerrado
 from service_manager.poblar_dims_sm import dimensiones_sm_backlog, dimensiones_sm_cerrado
@@ -14,22 +14,22 @@ import time
 import datetime
 
 
-# def clic():
-#     clic_abiertos()
-#     clic_resueltos()
-#     dimensiones_clic_abiertos()
-#     dimensiones_clic_resueltos()
-#     fact_clic_abiertos()
-#     fact_clic_resueltos()
+def clic():
+    clic_abiertos()
+    clic_resueltos()
+    dimensiones_clic_abiertos()
+    dimensiones_clic_resueltos()
+    fact_clic_abiertos()
+    fact_clic_resueltos()
 
 
-# def avaya():
-#     av_llamadas()
-#     av_abandonos()
-#     dims_avaya_llamadas()
-#     dims_avaya_abandonos()
-#     fact_av_llamadas()
-#     fact_av_abandonos()
+def avaya():
+    av_llamadas()
+    av_abandonos()
+    dims_avaya_llamadas()
+    dims_avaya_abandonos()
+    fact_av_llamadas()
+    fact_av_abandonos()
 
 
 def services_manager():
